3031daacc460a7dec635f8c71545835e5867f9d1
Aaron Krause <aaronjkrause@gmail.com>
2015-03-26 02:51:59 -0700

    FIRST TEST WORKS

--- test/home-view-test.js
+++ test/home-view-test.js
@@ -1,40 +1,44 @@
+'use strict';
+
 var React = require('react/addons');
-var assert = require('assert');
+var expect = require('chai').expect;
 var HomeView = require('../app/js/test-components/home-view');
 var TestUtils = React.addons.TestUtils;
-    // Since we're not using JSX here, we need to wrap the component in a factory
-    // manually. See https://gist.github.com/sebmarkbage/ae327f2eda03bf165261
-var HomeViewFactory = React.createFactory(HomeView);
+var HomeViewFactory = React.createFactory(HomeView)();
 
 describe('Todo-item component', function(){
+  var anchors;
+  var renderedComponent;
 
   before('render and locate element', function() {
-
     // Create our component
-    var component = HomeView;
+    var component = HomeViewFactory
 
-    // We want to render into the <body> tag
-    var renderTarget = document.body;
+    renderedComponent = TestUtils.renderIntoDocument(HomeViewFactory, document.body);
 
-    var renderedComponent = React.render(component, renderTarget);
-
-    // Searching for <input> tag within rendered React component
-    // Throws an exception if not found
-    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+    anchors = TestUtils.scryRenderedDOMComponentsWithTag(
       renderedComponent,
-      'nav'
+      'a'
     );
-
-    this.inputElement = inputComponent.getDOMNode();
+    // this.inputElement = inputComponents[0].getDOMNode()
+    // console.log(inputComponents[0].props.name)
+    // this.inputElementTwo = inputComponents[1].getDOMNode();
+    // console.log(renderedComponent.state);
 
   });
 
-  it('<input> should be of type "checkbox"', function() {
-    assert(this.inputElement.getAttribute('type') === 'checkbox');
-  });
+  after(function(){
+    this.inputElement = null;
+  })
 
-  it('<input> should not be checked', function() {
-    assert(this.inputElement.checked === false);
+  it('first anchor should have a name home', function() {
+    expect(anchors[0].props.name).to.eql('home');
   });
+  it('second anchor should have a name about', function() {
+    expect(anchors[1].props.name).to.eql('about')
+  });
+  it('should have a default view state of home', function() {
+    expect(renderedComponent.state.view).to.eql('home');
+  })
 
 })
