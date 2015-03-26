836ad4eee2106cef8e5d97d02e964be6f412dd68
Gaye Bulut <gayegul@gmail.com>
2015-03-25 23:44:40 -0700

    first draft UI styling

---
+++ app/js/components/home-content.jsx
@@ -0,0 +1,12 @@
+'use strict';
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <p>
+        This is our shiny attractive home page.
+      </p>
+    );
+  }
+});
