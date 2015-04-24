21ef003deaf73440465fe794381a651b16c9f039
Aaron Krause <aaronjkrause@gmail.com>
2015-04-23 18:41:17 -0700

    links added to list view

--- app/js/components/list-view.jsx
+++ app/js/components/list-view.jsx
@@ -6,19 +6,48 @@ var MovieList = require('./movie-list.jsx');
 
 //child of ControllerView
 var ListView = React.createClass({
-
+  getInitialState: function() {
+    return {displayList: null}
+  },
 
   componentDidMount: function() {
     UserActions.isValid(this.props.signedIn);
   },
 
+  handleClick: function(event) {
+    event.preventDefault();
+    var state = this.state;
+    console.log(event.target.dataset)
+    state.displayList = event.target.dataset.list;
+    this.setState(state)
+  },
+
+  clickBack: function() {
+    var state = this.state;
+    state.displayList = null;
+    this.setState(state);
+  },
+
   render: function() {
-    var movieLists = this.props.listData.map(function(list){
-      return <MovieList list={list} imageData={this.props.imageData}/>;
-    }.bind(this));
+    var displayList;
+    var backButton = <button onClick={this.clickBack}>Back</button>;
+
+    if (this.state.displayList) {
+      this.props.listData.forEach(function(list) {
+        if (list.id == this.state.displayList) {
+          displayList = <MovieList list={list} imageData={this.props.imageData}/>
+        }
+      }.bind(this))
+    }
+
+    var movieLinks = !(this.state.displayList) ? this.props.listData.map(function(list, i){
+      return <a style={{display: 'block'}} href='#' key={i} data-list={list.id}
+        onClick={this.handleClick}>{list.id}</a>
+    }.bind(this)) : null;
     return (
       <div>
-        {movieLists}
+        {movieLinks || backButton}
+        {displayList}
       </div>
     );
   }
